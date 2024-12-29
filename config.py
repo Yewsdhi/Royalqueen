06bd69efb821619801033078ee54ffd2aede1f0a
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2024-12-29 15:35:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SharaXOp/Musicbot",
+    "https://github.com/Yewsdhi/Royalqueen",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
