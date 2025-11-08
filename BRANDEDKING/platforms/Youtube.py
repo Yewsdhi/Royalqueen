674695dd36c87a5c68c6abf924d530fb5d75b671
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2025-11-08 09:21:47 +0530

    Refactor imports to use BRANDEDKING namespace

--- BRANDEDKING/platforms/Youtube.py
+++ BRANDEDKING/platforms/Youtube.py
@@ -8,9 +8,9 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from ShrutiMusic.utils.database import is_on_off
-from ShrutiMusic import app
-from ShrutiMusic.utils.formatters import time_to_seconds
+from BRANDEDKING.utils.database import is_on_off
+from BRANDEDKING import app
+from BRANDEDKING.utils.formatters import time_to_seconds
 import random
 import logging
 import aiohttp
